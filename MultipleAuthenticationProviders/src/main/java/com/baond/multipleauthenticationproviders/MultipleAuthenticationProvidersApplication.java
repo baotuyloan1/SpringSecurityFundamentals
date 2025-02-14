f5fc3a4d724478b316cd0f5e23c00d0fb1a54be4
baotuyloan <nguyenducbaokey@gmail.com>
2025-02-14 11:27:07 +0700

    multiple authentication providers

---
+++ MultipleAuthenticationProviders/src/main/java/com/baond/multipleauthenticationproviders/MultipleAuthenticationProvidersApplication.java
@@ -0,0 +1,13 @@
+package com.baond.multipleauthenticationproviders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleAuthenticationProvidersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleAuthenticationProvidersApplication.class, args);
+	}
+
+}
